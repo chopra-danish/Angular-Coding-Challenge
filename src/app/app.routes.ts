552e988a114549df0fe9f5ccbf1bede499f4904f
Danish Chopra <danishchopra11@gmail.com>
2024-12-11 11:57:39 +0530

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { TaskViewComponent } from './components/task-view/task-view.component';
+import { SeeTaskByIdComponent } from './components/see-task-by-id/see-task-by-id.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+
+export const routes: Routes = [
+
+    {
+        path: '', component:AppComponent, children:[
+            {
+                path:'', component:TaskViewComponent
+            },
+            {
+                path:'task-by-id', component:SeeTaskByIdComponent
+            },
+            {
+                path:'add-task', component:AddTaskComponent
+            },
+            {
+                path:'update', component:SeeTaskByIdComponent
+            }
+        ]
+    }
+];
